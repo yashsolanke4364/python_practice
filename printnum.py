0bac8bc5fc4dd6f39beb3531fac74e0e2f78f7dd
yash patil <yashsolanke4364@gmail.com>
2026-07-29 01:05:14 +0530

    initial commit

---
+++ printnum.py
@@ -0,0 +1,41 @@
+def print_1to100():
+ for i in range(0,101):
+    print(i)
+def print_even():
+  for i in range(0,101,2):
+    print(f" {i}")
+def odd_num():
+  for i in range(0,101):
+   if i%2!=0 :
+    print(i)  
+def table_of_num(num):
+    for i in range(1,11):
+        print(f"{num} * {i} = {num*i}")  
+def sum_of_num(n):
+  total = 0
+  for i in range(0,n):
+    total += i
+  print(total)
+
+def factorial(n):
+  if n < 0:
+    print("the factorial of the negative number is not possible")
+    return None
+  elif n == 0:
+    return 1
+  else:
+    result = 1
+    for i in range(1, n + 1):
+      result *= i
+    return result
+
+print_1to100()
+print_even()
+odd_num()
+num=int(input("Enter the number to print its table: "))
+table_of_num(num)
+n=int(input("enter the value of n:"))
+sum_of_num(n)
+x=int(input("enter the number to find factorial:"))
+print("the factorial of the number is :", factorial(x))
+
